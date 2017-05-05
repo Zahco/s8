6f04f0515bb857d1762e3c28bb07fd4018fe1d2f
Geoffrey <geoffrey.spaur@gmail.com>
2017-05-05 16:16:07 +0200

    Multi uri

--- AD/EIP/ZOO_MANAGER/src/main/java/tp/eip/WebIntegration.java
+++ AD/EIP/ZOO_MANAGER/src/main/java/tp/eip/WebIntegration.java
@@ -5,6 +5,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Expression;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.http.common.HttpOperationFailedException;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.dataformat.JaxbDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
@@ -20,8 +21,7 @@ import java.util.*;
  */
 public class WebIntegration {
 
-    public static void main(String[] args) throws Exception {
-
+    public static void findCoord() throws Exception {
         // Configure le logger par défaut
         BasicConfigurator.configure();
 
@@ -43,27 +43,87 @@ public class WebIntegration {
                         .unmarshal().json(JsonLibrary.Jackson)
                         .setHeader("longitude", simple("${body[position][longitude]}"))
                         .setHeader("latitude", simple("${body[position][latitude]}"))
-                        .log("reponse received : ${body}\n ${header.latitude} ${header.longitude}");
+                        .log("${header.latitude} ${header.longitude}");
             }
         };
         routeBuilderSwitch.addRoutesToCamelContext(context);
 
 
         // On démarre le contexte pour activer les routes
-        context.start(); 
+        context.start();
 
         // On crée un producteur
         ProducerTemplate pt = context.createProducerTemplate();
 
         String message = "Canine";
-        pt.sendBody("direct:TestWeb", message);
-//        Scanner scanner = new Scanner(System.in);
-//        while (true) {
-//            // Enter the name of an animal or close to exit
-//            //message = scanner.nextLine();
-//            if (message.equals("close")) break;
-//
-//        }
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // Enter the name of an animal or close to exit
+            message = scanner.nextLine();
+            if (message.equals("close")) break;
+            pt.sendBody("direct:TestWeb", message);
+        }
+    }
+
+    public static void searchWithManyInstance() throws Exception {
+        // Configure le logger par défaut
+        BasicConfigurator.configure();
+
+        // Contexte Camel par défaut
+        CamelContext context = new DefaultCamelContext();
+
+        // Recherche d'un animal par son nom
+        RouteBuilder routeBuilderSwitch = new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+                // Start MyServiceController before
+                from("direct:TestWeb")
+                        .setHeader("searched", simple("${body}"))
+                        .setHeader(Exchange.HTTP_METHOD,constant("POST"))
+                        .doTry()
+                            .setBody(header("searched"))
+                            .to("http://127.0.0.1:8080/animals/findByName/")
+                            .unmarshal().json(JsonLibrary.Jackson)
+                            .log("${body}")
+                        .doCatch(HttpOperationFailedException.class)
+                            .doTry()
+                                .setBody(header("searched"))
+                                .to("http://127.0.0.1:8081/animals/findByName/")
+                                .unmarshal().json(JsonLibrary.Jackson)
+                                .log("${body}")
+                            .doCatch(HttpOperationFailedException.class)
+                                .log("Animal introuvable")
+                            .end()
+                        .end()
+
+                ;
+            }
+        };
+        routeBuilderSwitch.addRoutesToCamelContext(context);
+
+
+        // On démarre le contexte pour activer les routes
+        context.start();
+
+        // On crée un producteur
+        ProducerTemplate pt = context.createProducerTemplate();
+
+        String message = "Canine";
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // Enter the name of an animal or close to exit
+            message = scanner.nextLine();
+            if (message.equals("close")) break;
+            pt.sendBody("direct:TestWeb", message);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //findCoord();
+        searchWithManyInstance();
     }
 
 }
